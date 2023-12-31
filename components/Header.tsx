b23a7ba0f467ebc0a1fe98d49d7ed8fc6baebb94
Alyssa Feola <65581330+its-a-lisa@users.noreply.github.com>
2023-12-30 19:49:11 -0500

    working dark mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,28 @@
 import * as React from "react";
 import {
   PlasmicHeader,
-  DefaultHeaderProps
+  DefaultHeaderProps,
 } from "./plasmic/its_a_lisa_site/PlasmicHeader";
 import { HTMLElementRefOf } from "@plasmicapp/react-web";
 import { useSelector } from "@plasmicapp/react-web/lib/host";
 
-export interface HeaderProps extends DefaultHeaderProps {
-  darkMode: boolean; // Add the 'darkMode' prop
-  toggleDarkMode: () => void; // Add the 'toggleDarkMode' prop
-}
+export interface HeaderProps extends DefaultHeaderProps {}
 
 function Header_(props: HeaderProps, ref: HTMLElementRefOf<"div">) {
-  const changeMode = useSelector("changeMode");
-  const { darkMode, toggleDarkMode, ...restProps } = props;
+  const { darkMode, toggleDarkMode } = useSelector("darkModeData");
 
-
-  return <PlasmicHeader 
-  header={{ ref }} _switch={{
-    onChange: (isChecked) => {
-      toggleDarkMode(); // Toggle dark mode when the switch changes
-      changeMode(isChecked ? "dark" : undefined);
-    },
-    // Pass the value of 'darkMode' to the switch component
-    checked: darkMode,
-  }}
-  {...restProps} 
-  />;
+  return (
+    <PlasmicHeader
+      header={{ ref }}
+      themeSwitch={{
+        onChange: () => {
+          toggleDarkMode();
+        },
+        // Pass the value of 'darkMode' to the switch component
+        checked: darkMode,
+      }}
+    />
+  );
 }
 
 const Header = React.forwardRef(Header_);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,32 @@
+import { DataProvider } from "@plasmicapp/react-web/lib/host";
 import "../styles/globals.css";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useState } from "react";
+import ThemeContext from "@/components/plasmic/its_a_lisa_site/PlasmicGlobalVariant__Theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [darkMode, setDarkMode] = useState(false);
 
-   // Toggle the theme mode
-   const toggleDarkMode = () => {
+  // Toggle the theme mode
+  const toggleDarkMode = () => {
     setDarkMode((prevMode) => !prevMode);
   };
 
   return (
-     
-    <PlasmicRootProvider
-      Head={Head}
-      globalVariants={[
-        // Set the "Theme" variant based on darkMode state
-        { name: "Theme", value: darkMode ? "dark" : undefined },
-      ]}
+    <DataProvider
+      name="darkModeData"
+      data={{
+        darkMode,
+        toggleDarkMode,
+      }}
     >
-      <Component {...pageProps} darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-    </PlasmicRootProvider>
+      <ThemeContext.Provider value={darkMode ? "dark" : undefined}>
+        <PlasmicRootProvider Head={Head}>
+          <Component {...pageProps}/>
+        </PlasmicRootProvider>
+      </ThemeContext.Provider>
+    </DataProvider>
   );
 }
